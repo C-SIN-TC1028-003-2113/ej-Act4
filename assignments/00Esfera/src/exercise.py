c4c62ac5e88219c7037473a9fc2b287c5810de50
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2021-08-19 16:44:20 -0600

    Initial commit

---
+++ assignments/00Esfera/src/exercise.py
@@ -0,0 +1,18 @@
+import math
+
+def main():
+    #escribe tu código abajo de esta línea
+    print("Este programa calcula el área y el volumen de una esfera")
+
+    radio = float(input("Dame el valor del radio: "))
+
+    area = 4 * math.pi * math.pow(radio,2)
+    volumen = (4/3) * math.pi * math.pow(radio,3)
+
+    print(f"El area de la esfera es: {area}")
+    print(f"El volumen de la esfera es: {volumen}")
+
+    print("Termine...")
+    
+if __name__=='__main__':
+    main()
